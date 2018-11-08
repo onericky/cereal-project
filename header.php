a29f1e98c1846d976c0247f3b54be255c5c6e88f
Ricky <onericky16@hotmail.com>
2018-11-08 00:58:55 -0600

    change title and icon in header

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <link rel="stylesheet" type="text/css" href="./assets/slick/slick.css"/>
 	<link rel="stylesheet" type="text/css" href="./assets/slick/slick-theme.css"/>
 	<link rel="stylesheet" type="text/css" href="./assets/css/style.css"/>
+	<link rel="icon" href="./assets/images/logo.png">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
@@ -12,23 +13,22 @@
 	<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 	<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 	<script type="text/javascript" src="./assets/slick/slick.min.js"></script>
-	<title>Cereals & Milks</title>
+	<title>Yumyum Bar</title>
 </head>
 <body>
 	<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-	<!-- Brand/logo -->
-	<a class="navbar-brand" href="./">
-	<img src="./assets/images/logo.png" alt="logo" style="width:40px;">
-	</a>
+		<a class="navbar-brand" href="./">
+			<img src="./assets/images/logo.png" alt="logo" style="width:40px;">
+			Yumyum Bar
+		</a>
 
-	<!-- Links -->
-	<ul class="navbar-nav">
-		<li class="nav-item">
-			<a class="nav-link" href="./">Inicio</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="./about.php">Nosotros</a>
-		</li>
-	</ul>
+		<ul class="navbar-nav">
+			<li class="nav-item">
+				<a class="nav-link" href="./">Inicio</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="./about.php">Nosotros</a>
+			</li>
+		</ul>
 	</nav>
-</body>
+</body>	
