c6d3581a0c9b5cc4c009f0228a8df187e555c908
Ricky Castro <rcastro@zegucom.com.mx>
2018-11-09 16:51:48 -0600

    add connection to host

--- connection.php
+++ connection.php
@@ -1,10 +1,20 @@
 <?php 
 $servername = "localhost";
-//$username = "root";
-//$password = "";
+$username = "id7781225_yumyumbar";
+$password = "castro95";
+$database = "id7781225_yumyumbar";
+
+/* laptop
+$username = "root";
+$password = "";
+$database = "project_cereals";
+*/
+
+/* homestead
 $username = "homestead";
 $password = "secret";
 $database = "project_cereals";
+*/
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
